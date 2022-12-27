565a1fb00699837516639c5013c464da25624bd7
Mykola-Bohdan Vynnytskyi <qbodia@gmail.com>
2022-12-27 20:16:47 +0200

    Add createUser functionality

--- src/main/java/com/mbvyn/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/mbvyn/photoapp/api/users/ui/controllers/UsersController.java
@@ -3,8 +3,14 @@ package com.mbvyn.photoapp.api.users.ui.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mbvyn.photoapp.api.users.ui.model.UserRequestModel;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -16,5 +22,10 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port " + env.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String createUser(@Valid @RequestBody UserRequestModel userDetails) {
+		return "Create user method is called";
 	}
 }
